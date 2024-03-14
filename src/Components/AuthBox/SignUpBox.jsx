5e63b36c11e1054d303f249306ed7a482b93731c
unknown <sharinhansha@gmail.com>
2024-03-14 14:09:30 +0530

    signup and login

--- src/Components/AuthBox/SignUpBox.jsx
+++ src/Components/AuthBox/SignUpBox.jsx
@@ -1,29 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SignUpBox.css'
 import CustomInput from '../Common/CustomInput/CustomInput'
 
 function SignUpBox({setBoxType}) {
+  const [signupData,setSignupData]=useState({})
+  const handleChange=(e)=>{
+    //   console.log(e)
+   setSignupData(...signupData,{[e.target.name]:e.target.value})
+  }
+  const doSignUp=()=>{
+    
+  }
   return (
     <div>
       
    <>
    <div className='d-flex flex-column'>
     <div className='mt-4'>
-      <CustomInput label={'Name'} value={null}/>
+      <CustomInput label={'Name'} name={'name'} value={signupData.name} onchange={handleChange}/>
     </div>
     <div className='mt-4'>
-    <CustomInput label={'Email'} value={null}/>
+    <CustomInput label={'Email'} name={'email'} value={signupData.email} onchange={handleChange}/>
   </div>
   <div className='mt-4'>
-      <CustomInput label={'Mobile Number'} value={null}/>
+      <CustomInput label={'Mobile Number'} name={'phone'} value={signupData.phone} onchange={handleChange}/>
     </div>
     <div className='mt-4'>
-      <CustomInput label={'Passworrd'} value={null}/>
+      <CustomInput label={'Password'} name={'password'} value={signupData.password} onchange={handleChange}/>
     </div>
     <div className='mt-4'>
-      <CustomInput label={'Confirm Pasword'} value={null}/>
+      <CustomInput label={'Confirm Pasword'} name={'confirmPassword'} value={signupData.confirmPassword} onchange={handleChange}/>
     </div>
-  <button className='common-button mt-4 align-self-center'>SIGN UP</button>
+  <button className='common-button mt-4 align-self-center' onClick={doSignUp}>SIGN UP</button>
   <p className='already-account mt-4'>
 Already Have An Account <i onClick={()=>setBoxType('login')}> login here </i>
   </p>
